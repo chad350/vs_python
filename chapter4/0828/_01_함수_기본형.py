bf210b66a9a0b449b6ae53df7f93451f9ebc1901
chad350 <yyc2999@gmail.com>
2026-08-28 13:58:53 +0900

    함수 수업 내용 업데이트

---
+++ chapter4/0828/_01_함수_기본형.py
@@ -0,0 +1,54 @@
+def error_msg() :
+    print("\033[35mError\033[0m")
+
+
+# 프로그램 1
+print("\033[35mError\033[0m")
+print("계정을 확인해주세요.")
+
+print("\033[31mError\033[0m")
+print("비밀번호를 확인해 주세요.")
+
+print("\033[35mError\033[0m")
+print("없는 계정입니다..")
+
+
+
+
+# 프로그램 2
+error_msg()
+print("계정을 확인해주세요.")
+
+error_msg()
+print("비밀번호를 확인해 주세요.")
+
+error_msg()
+print("없는 계정입니다..")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ chapter4/0828/_02_함수_매개변수.py
@@ -0,0 +1,12 @@
+#        파라미터 / 매개변수
+def error_msg( var ) :    
+    print("\033[35mError", var, "\033[0m")
+
+
+# Error 버그가 감지되었습니다.
+# Error 서버가 공격받았습니다.
+
+
+#           아규먼트 / 인자값
+error_msg( "버그가 감지되었습니다." ) 
+error_msg( "서버가 공격받았습니다." ) 
